7b8964d07576bc942bd2f0cdfd986a59652123b8
caprav <vines07@gmail.com>
2023-06-27 10:47:55 -0400

    shot potatos destroying once they reach camera edges

--- Assets/Scripts/shotPotato.cs
+++ Assets/Scripts/shotPotato.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.UI;
+using Matrix4x4 = UnityEngine.Matrix4x4;
+using Vector3 = UnityEngine.Vector3;
 
 public class shotPotato : MonoBehaviour
 {
     Player player;
+    Camera camera;
     private int shotDirection; // 1 for to the right, -1 for to the left
     private float potatoShotForce = 20.0f;
     public int enemiesKilled = 0;
@@ -13,13 +17,18 @@ public class shotPotato : MonoBehaviour
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
-        
+        camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         //need to determine if this potato is going to the right or left
         shotDirection = player.playerFaceDirection;
     }
 
     public delegate void DecrementMonsterEventHandler(string tag);
     public static event DecrementMonsterEventHandler MonsterDecrementEvent;
+
+    //These variables are used to detect screen edge in environment 
+    public Matrix4x4 cameraPos;
+    private float screenLeftEdge;
+    private float screenRightEdge;
 
     // Start is called before the first frame update
     void Start()
@@ -49,11 +58,24 @@ public class shotPotato : MonoBehaviour
             Debug.Log("The tater did not get the direction... womp womp!");
     }
 
+    // VPC 6/27 - this checks if a fired potato reaches the edge of the camera and destroys it if so
     void checkBoundaries()
     {
-        // VPC 6/21 - We should fill in this to check if the projectile has reached the edge of the camera view
-        // We probably do not want the ammo rounds to continue off screen and kill enemies that the 
-        // player cannot see. 
+        cameraPos = camera.cameraToWorldMatrix;
+        //Debug.Log("Camera transform matrix = " + cameraPos.ToString());
+
+        float checkval = cameraPos.GetRow(0).w; //VPC 6/27 - not sure why the x value is stored as
+                                                //'w' in this matrix, but can't find much documentation
+                                                //Debug.Log("right side = " + (checkval+10) + "    left side = " + (checkval-10));
+
+        screenLeftEdge = checkval - 10;
+        screenRightEdge = checkval + 10;
+        float curPosition = this.transform.position.x;
+        if (curPosition > screenRightEdge || curPosition < screenLeftEdge)
+        {
+            Destroy(this.gameObject);
+        }
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
